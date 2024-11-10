1c490a233b81d2eb20030fa09540c06eae5830df
doworder <doworder@gmail.com>
2024-11-10 20:33:59 +0300

    Add mask function Add input validation Add settings file

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
-is_gameover = False  # TODO кажется это лишнее
+import configparser
+import random
+
+
+def mask(string: str):
+    return [" _ " * len(string)]
+
+
+def input_validation(simbol, locale):
+    if "а" <= simbol <= "я" or simbol == "ё":
+        return True
+    else:
+        return False
 
 
 if __name__ == "__main__":
-    while not is_gameover:
-        user_answer = input("Сыграем?(д/н):")
-        if user_answer == "n" or user_answer == "н":
-            is_gameover = True
-            break
+    try:
+        config = configparser.ConfigParser()  # parser object
+        config.read("settings.ini")
 
-        errors_count = 0
-        while errors_count < 6:
-            print(f"Количество ошибок: ", errors_count)
-            errors_count += 1
+        while True:
+            user_answer = input("Сыграем?(д/н):")
+            if user_answer == "n" or user_answer == "н":
+                # is_gameover = True
+                break
 
-    exit(0)
+            with open(config["Settings"]["dictionary"]) as file:
+                word = random.choice(file.readlines())
+
+            errors_count = 0
+            while errors_count < 6:
+                print(*mask(word))
+                letter = input("Введите букву: ")
+                if not input_validation(letter, config["Settings"]["locale"]):
+                    continue
+                if letter in word:
+                    print(mask(word))
+                else:
+                    errors_count += 1
+                print(f"Количество ошибок: ", errors_count)
+
+    except Exception as e:
+        print("Возникла ошибка", e)
+
+    finally:
+        exit(0)
