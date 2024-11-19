a7eb7312ced4ee0fc319a86b06487d1c8096c0f9
doworder <doworder@gmail.com>
2024-11-19 21:09:29 +0300

    refactor:  add read setting functions  add get_word function  rename word -> hidden_word

--- main.py
+++ main.py
@@ -3,49 +3,72 @@ import random
 import graphics
 
 
+def get_config(path):
+    # if not os.path.exists(path):
+    #     create_config(path)
+
+    config = configparser.ConfigParser()
+    config.read(path)
+    return config
+
+
+def get_setting(path, section, setting):
+    config = get_config(path)
+    value = config.get(section, setting)
+    # msg = "{section} {setting} is {value}".format(
+    #     section=section, setting=setting, value=value
+    # )
+    #
+    # print(msg)
+    return value
+
+
+def get_word(dictionary):
+    with open(dictionary) as file:
+        word = random.choice(file.read().splitlines())
+    return word
+
+
 def update_mask(string: str):
-    for i, item in enumerate(word):
+    for i, item in enumerate(hidden_word):
         if string == item:
             hidden_mask[i] = string
 
 
-def input_validation(string, locale):
-    if len(string) == 1 or len(string) == len(word):
+def input_validation(string):
+    if len(string) == 1 or len(string) == len(hidden_word):
         if "а" <= string <= "я" or string == "ё":
             return True
     else:
         return False
 
 
-def hangman_rendering(namber):
-    print(graphics.hangman_graphics[namber])
+def hangman_rendering(state):
+    print(graphics.hangman_graphics[state])
 
 
 if __name__ == "__main__":
     try:
-        config = configparser.ConfigParser()  # parser object
-        config.read("settings.ini")
-
         while True:
             user_answer = input("Сыграем?(д/н):").lower()
             if user_answer == "n" or user_answer == "н":
                 break
 
-            with open(config["Settings"]["dictionary"]) as file:
-                word = random.choice(file.read().splitlines())
+            game_dictionary = get_setting("settings.ini", "Settings", "dictionary")
+            hidden_word = get_word(game_dictionary)
 
             errors_count = 0
             entered_letters = []
-            hidden_mask = [" _ "] * len(word)
+            hidden_mask = [" _ "] * len(hidden_word)
             while errors_count < 6:
                 hangman_rendering(errors_count)
                 print(*hidden_mask)
                 letter = input("Введите букву: ").lower()
-                if not input_validation(letter, config["Settings"]["locale"]):
+                if not input_validation(letter):
                     continue
-                if letter == word:
+                if letter == hidden_word:
                     break
-                if letter in word:
+                if letter in hidden_word:
                     update_mask(letter)
                 else:
                     if letter not in entered_letters:
@@ -60,12 +83,13 @@ if __name__ == "__main__":
                 hangman_rendering(errors_count)
                 print("Вы проиграли!")
                 print("Использованные буквы: ", entered_letters)
-                print("Загаданное слово: ", word.upper())
+                print("Загаданное слово: ", hidden_word.upper())
             else:
                 print("Победа!", "Вы отгадали слово", sep="\n")
 
     except Exception as e:
         print("Возникла ошибка", e)
+        exit(1)
 
     finally:
         exit(0)
