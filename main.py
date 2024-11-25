fa207bc36a352799f3766432e03f4e45ca2adb63
doworder <doworder@gmail.com>
2024-11-25 10:33:59 +0300

    State for publication fo review

--- main.py
+++ main.py
@@ -32,27 +32,14 @@ def input_validation(string: str, hidden_word: str) -> bool:
     if len(string) == 1 or len(string) == len(hidden_word):
         if "а" <= string <= "я" or string == "ё":
             return True
+        else:
+            print("Введите букву русского алфавита")
+            return False
     else:
-        print("Введите букву русского алфавита")
+        print("Введите одну букву")
         return False
 
 
-# def user_input(hidden_word: str, hidden_mask: list, entered_letters: list, errors_count: int) -> str:
-#     letter = input("Введите букву: ").lower()
-#     if not input_validation(letter, hidden_word):
-#         continue
-#     if letter == hidden_word:
-#         break
-#     if letter in hidden_word:
-#         hidden_mask = update_mask(letter, hidden_word, hidden_mask)
-#     else:
-#         if letter not in hidden_mask:
-#             errors_count += 1
-#             entered_letters.append(letter)
-#     print("Количество ошибок: ", errors_count)
-#     return letter
-#
-#
 def hangman_rendering(state: int) -> None:
     print(graphics.hangman_graphics[state])
 
