21cd0c436a40506cedf873cb359043bbf5413a62
doworder <doworder@gmail.com>
2025-02-25 11:43:36 +0300

    refactor vertical tabs

--- src/hangman/main.py
+++ src/hangman/main.py
@@ -20,10 +20,12 @@ def get_word(dictionary_path: str) -> str:
     try:
         with open(dictionary_path) as file:
             word: str = choice(file.read().splitlines())
+
         return word
 
     except FileNotFoundError:
         print('Не найден файл со словами для игры! Обратитесь к разработчику')
+
         return ''
 
 
@@ -44,6 +46,7 @@ def is_hidden_word(user_input: str, hidden_word: str) -> bool:
 def is_one_symbol(user_input: str) -> bool:
     if len(user_input) != 1:
         print("Введите одну букву")
+
         return False
 
     else:
@@ -53,6 +56,7 @@ def is_one_symbol(user_input: str) -> bool:
 def is_entered_letters(user_input: str, entered_letters: set) -> bool:
     if user_input in entered_letters:
         print("Вы уже вводили эту букву!")
+
         return True
 
     else:
@@ -103,22 +107,27 @@ def play_game(hidden_word: str) -> None:
     while MASK_SYMBOL in str(mask) and errors_count < 6:
         show_hangman_state(errors_count)
         print(*mask)
+
         letter: str = make_input(entered_letters, hidden_word)
         entered_letters.add(letter)
         if is_hidden_word(letter, hidden_word):
             break
+
         if letter in hidden_word:
             update_mask(letter, hidden_word, mask)
+
         else:
             errors_count += 1
 
         print("Количество ошибок: ", errors_count)
+
     print_end_state(errors_count, entered_letters, hidden_word)
 
 
 def start_game() -> None:
     while True:
         user_answer: str = input("Сыграем? (Нажмите любую клавишу для продолжения ИЛИ н - для выхода): ").lower()
+
         if user_answer == "n" or user_answer == "н":
             break
 
@@ -133,6 +142,7 @@ def print_end_state(state: int, used_letters: set[str], hidden_word: str) -> Non
         print("Вы проиграли!")
         print("Использованные буквы: ", used_letters)
         print("Загаданное слово: ", hidden_word.upper())
+
     else:
         print("Победа!", f"Вы отгадали слово: {hidden_word.upper()}", sep="\n")
 
@@ -142,6 +152,7 @@ if __name__ == "__main__":
         SETTINGS_FILE: str = "config.ini"
         MASK_SYMBOL: str = get_setting(SETTINGS_FILE, "Settings", "mask_symbol")
         GAME_DICTIONARY_NAME: str = get_setting("config.ini", "Settings", "dictionary")
+
         start_game()
 
     except Exception as e:
